f976e5d5fdf0fc5ec5e4991388af71e7e567a910
swords <15902110521@163.com>
2018-04-19 11:01:37 +0800

    4/19 findMinAndMax

---
+++ findMinAndMax.py
@@ -0,0 +1,23 @@
+def findMinAndMax(nums):
+    if nums == []:
+        return(None,None)
+    else:
+        ma = nums[0]
+        mi = nums[0]
+        for n in nums:
+            if n> ma:
+                ma = n
+            if n< mi:
+                mi = n 
+        return(mi,ma)
+# 测试
+if findMinAndMax([]) != (None, None):
+    print('测试失败!')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失败!')
+else:
+    print('测试成功!')
