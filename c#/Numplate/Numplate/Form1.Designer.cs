ef1035f766967b9aee191c31f4715d1a662815c6
swords <15902110521@163.com>
2018-08-07 20:24:45 +0800

    sobel算子

--- c#/Numplate/Numplate/Form1.Designer.cs
+++ c#/Numplate/Numplate/Form1.Designer.cs
@@ -31,16 +31,18 @@
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(690, 303);
+            this.button1.Location = new System.Drawing.Point(713, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -50,22 +52,13 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(690, 363);
+            this.button2.Location = new System.Drawing.Point(713, 41);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "识别";
+            this.button2.Text = "高斯模糊";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(672, 426);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
             // 
             // openFileDialog1
             // 
@@ -73,26 +66,68 @@
             // 
             // imageBox1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(690, 49);
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(87, 143);
+            this.imageBox1.Size = new System.Drawing.Size(672, 426);
             this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(713, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "灰度化";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(713, 100);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "边缘检测";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(713, 129);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "二值化";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(713, 159);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "闭操作";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.imageBox1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "plate identify";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -102,9 +137,12 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- c#/Numplate/Numplate/Form1.cs
+++ c#/Numplate/Numplate/Form1.cs
@@ -17,39 +17,131 @@ namespace Numplate
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
         }
-        byte[] picturebytes;
+        //byte[] picturebytes;
         private void button1_Click(object sender, EventArgs e)
         {
-            openFileDialog1.Filter = "*.jpg|*.JPG|*.gif|*.GIF|*.bmp|*.BMP";
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            OpenFileDialog ofdOpenLocalImage = new OpenFileDialog();
+            ofdOpenLocalImage.InitialDirectory = @"C:\Users\Public\Pictures"; //默认路径
+            ofdOpenLocalImage.Title = "选择图片";
+            ofdOpenLocalImage.Multiselect = true;
+            ofdOpenLocalImage.Filter = "jpg图片|*.jpg|jpeg图片|*.jpeg|bmp图片|*.bmp|png图片|*.png|pdf文件|*.pdf";
+            ofdOpenLocalImage.CheckFileExists = true;
+            ofdOpenLocalImage.Multiselect = false;
+            if (ofdOpenLocalImage.ShowDialog(this) == DialogResult.OK)
             {
-                string fullpath = openFileDialog1.FileName;
-                FileStream fs = new FileStream(fullpath, FileMode.Open);
-                picturebytes = new byte[fs.Length];
-                BinaryReader br = new BinaryReader(fs);
-                picturebytes = br.ReadBytes(Convert.ToInt32(fs.Length));
-                MemoryStream ms = new MemoryStream(picturebytes);
-                Bitmap bmpt = new Bitmap(ms);
-                pictureBox1.Image = bmpt;
-
+                Image<Bgr, byte> image = new Image<Bgr, byte>(ofdOpenLocalImage.FileName);
+                imageBox1.Image = image;
+            }
+            else
+            {
+                MessageBox.Show("未选择图片");
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            //Image<Bgr, Byte> image = new Image<Bgr, byte>(@"D:\src.jpg");//从文件加载图片
+            //imageBox1.Image = image;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Emgu.CV.Image<Bgr, byte> image = new Emgu.CV.Image<Bgr, byte>(320, 240, new Bgr(0, 255, 255));
-            imageBox1.Image = image;
+            if (imageBox1.Image != null)
+            {
+                var image = (Image<Bgr, Byte>)imageBox1.Image;
+                imageBox1.Image = image.SmoothGaussian(5);
+
+            }
+            else
+            {
+                MessageBox.Show("请先导入图片");
+            }
         }
 
-        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (imageBox1.Image != null)
+            {
+                var image = (Image<Bgr, Byte>)imageBox1.Image;
+                Image<Gray, Byte> Gr_img = image.Convert<Gray, Byte>();
+                imageBox1.Image = Gr_img;
+
+            }
+            else
+            {
+                MessageBox.Show("请先导入图片");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (imageBox1.Image != null)
+            {
+                var a = (Image<Gray, Byte>)imageBox1.Image;
+                imageBox1.Image = a.Sobel(1, 0, 3).AddWeighted(a.Sobel(0, 1, 3), 0.5, 0.5, 0);
+
+            }
+            else
+            {
+                MessageBox.Show("请先导入图片");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (imageBox1.Image != null)
+            {
+                var image = (Image<Gray, Single>)imageBox1.Image;
+                var threshImage = image.CopyBlank();
+                CvInvoke.Threshold(image, threshImage, 150, 255, ThresholdType.Binary);
+                imageBox1.Image = threshImage;
+
+                    
+            }
+            else
+            {
+                MessageBox.Show("请先导入图片");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
+
+namespace EasyPR
+{
+    class CPlateLocate
+    {
+        public CPlateLocate()
+        { 
+
+
+        }
+
+        //! 高斯模糊所用变量  
+        public static int m_GaussianBlurSize = 5;
+
+        //! 连接操作所用变量  
+        protected int m_MorphSizeWidth;
+        protected int m_MorphSizeHeight;
+
+        //! verifySize所用变量  
+        protected float m_error;
+        protected float m_aspect;
+        protected int m_verifyMin;
+        protected int m_verifyMax;
+
+        //! 角度判断所用变量  
+        protected int m_angle;
+
     }
 }
